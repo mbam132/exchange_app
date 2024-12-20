4235df0f4690e174366b81c25adbc734c18fcb9e
Francisco Guzman <fguzman53000@gmail.com>
2024-12-20 12:16:41 -0400

    types

---
+++ lib/types/index.dart
@@ -0,0 +1,14 @@
+class Currency {
+  final String symbol;
+  final String symbol2;
+  final String name;
+  final String id;
+  final String imagePath;
+
+  Currency(
+      {required this.symbol,
+      required this.symbol2,
+      required this.name,
+      required this.id,
+      required this.imagePath});
+}
